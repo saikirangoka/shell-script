2af04cd9224ffa0875326e0797f1bbdd38fb96c5
saikirangoka <gokasaikiran15@gmail.com>
2025-04-09 16:04:51 +0530

    conversation added

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Jagadeesh: Hi jaandu"
+echo "jandu: Hello jagadeesh"
+echo "jagdeesh: How are you are you fine"
+echo "jaandu: iam fine you are smart"
+echo "jagadeesh: Thank you"
