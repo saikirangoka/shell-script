1fa44373920d68e900dd82c791041bee91fa4ea0
saikirangoka <gokasaikiran15@gmail.com>
2025-04-10 11:21:47 +0530

    conversation added

---
+++ 02_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is normal conversation
+
+$PERSON1=saikiran
+$PERSON2=jagadeesh
+
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON1 : Ooh Hi who are you bathu bacha "
+echo "$PERSON1 : me only ra gedhi bacha $PERSON2"
