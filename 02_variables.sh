f7ae7ea9ef66117ad825babec836e0b94c8e8c42
saikirangoka <gokasaikiran15@gmail.com>
2025-04-10 11:30:46 +0530

    Hi

--- 02_variables.sh
+++ 02_variables.sh
@@ -6,5 +6,5 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1 : Hi $PERSON2"
-echo "$PERSON1 : Ooh Hi who are you bathu bacha "
+echo "$PERSON2 : Ooh Hi who are you bathu bacha "
 echo "$PERSON1 : me only ra gedhi bacha $PERSON2"
