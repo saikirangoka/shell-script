3c4b8c071a4a26bd1054fd6ef86cb5540214d1e4
saikirangoka <gokasaikiran15@gmail.com>
2025-04-10 11:13:57 +0530

    Hi

--- 01_hello-world.sh
+++
@@ -1,6 +0,0 @@
- #! /bin/bash
-
- # This line is committed
-
- echo "Hello sai"
- echo "This is updated"
--- variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-echo "Jagadeesh: Hi jaandu"
-echo "jandu: Hello jagadeesh"
-echo "jagdeesh: How are you are you fine"
-echo "jaandu: iam fine you are smart"
-echo "jagadeesh: Thank you"
