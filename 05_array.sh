064867b75eecab52f1e9f8ada43247d3ca5182fb
saikirangoka <gokasaikiran15@gmail.com>
2025-04-13 20:34:55 +0530

    Hi

--- 05_array.sh
+++ 05_array.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-NUMBER1=$1
-NUMBER2=$2
+CARS=("MERCEDES" "AUDI" "BMW")
+#index starts from zero
 
-TIMESTAMP=$(date)
-
-SUM=$(($NUMBER1+$NUMBER2))
-
-echo "script executed at : $TIMESTAMP"
-echo "sum of two numbers : $SUM"
-echo "sum of $1 and $2 : $SUM"
+echo "First movie : ${CARS[0]}
+echo "All movies : ${CARS[@]}
+echo "Second Movie : ${CARS[1]}
+echo "Third movie : ${CARS[2]}
