e3840de74926d79aa958b83743ea6104cb275e97
saikirangoka <gokasaikiran15@gmail.com>
2025-04-14 01:46:12 +0530

    Hi

--- 08_install_script.sh
+++ 08_install_script.sh
@@ -7,21 +7,52 @@ then
     echo "you must have sudo access to execute the script"
 fi
 
-dnf install mysql -y
-
+dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "installing mysql... is fail"
-    exit 1
+    dnf install mysql -y
+
+        if [ $? -ne 0 ]
+        then
+            echo "installing mysql... is fail"
+            exit 1
+        else
+            echo "installing mysql... is success"
+        fi
 else
-    echo "installing mysql... is success"
+    echo "mysql is already installed"
 fi
 
+
+
+dnf list installed git
+
+if [ $? -ne 0 ]
 dnf install git -y
-if [ $? -ne 0 ]
-then
-    echo "installing git... is fail"
-    exit 1
+    if [ $? -ne 0 ]
+    then
+        echo "installing git.. is fail"
+        exit 1
+    else
+        echo "installing git.. is success"
+    fi
 else
-    echo "installinf git... is success"
+    echo "git is already installed"
 fi
+
+
+#then
+#   echo "mysql.. will be install"
+#else
+#    echo "mysql is already installed"
+
+
+
+#dnf install git -y
+#if [ $? -ne 0 ]
+#then
+#    echo "installing git... is fail"
+#    exit 1
+#else
+#    echo "installinf git... is success"
+#fi
