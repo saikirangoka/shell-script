eba32e283a519ca5fd7697423a78a6a0f85fa806
saikirangoka <gokasaikiran15@gmail.com>
2025-04-17 01:08:18 +0530

    Hi

--- 14_loops.sh
+++ 14_loops.sh
@@ -1,6 +1,61 @@
 #!/bin/bash
 
-for i in {0..10}
+USERIDII=$(id -u)
+if [ $USERIDII -ne 0 ]
+then
+    echo "To execute this command you need root access"
+    exit 1
+fi
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+LOGS_FOLDER="/var/logs/shellscript-log"
+mkdir -p "$LOGS_FOLDER"
+LOGS_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+REPEATED(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "installing $2... is $R fail $W"
+        exit 1
+    else
+        echo -e "installing $2... is $G success $W"
+    fi
+}
+
+echo "script started executed at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    REPEATED $? mysql
+else
+    echo -e "mysql is already $Y installed $W"
+fi
+
+dnf list installed git &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    REPEATED $? git
+else
+    echo -e "git is already $Y installed $W"
+fi
+
+for package in $@
 do
-    echo $i
+    dnf list installed $package &>>$LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y &>>$LOG_FILE_NAME
+        VALIDATE() $? $package
+    else
+        echo -e "$package already $Y installed $W"
+    fi
 done
