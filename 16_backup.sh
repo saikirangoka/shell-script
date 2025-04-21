d12dee6ccbc1267515e4ef1d0ab8e42247be79da
saikirangoka <gokasaikiran15@gmail.com>
2025-04-21 13:31:29 +0530

    Hi

---
+++ 16_backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ R="\e[31m"
+ G="\e[32m"
+ Y="\e[33m"
+ N="\e[0m"
+
+ SOURCE_DIRECTORY=$1
+ DESTINATION_DIRECTORY=$2
+ DAYS=${3:-14}
+
+ LOGS_FOLDER="/var/logs/expense-logs"
+ mkdir -p "$LOGS_FOLDER"
+ LOG_FILE=$(echo $0 | cut -d "." -f1 )
+ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+ VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R Fail $N"
+        exit 1
+    else
+        echo -e "$2 is $G Success $N"
+    fi
+ }
+
+ echo "Script satrted running at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+ USAGE(){
+    echo -e " $R USAGE : $N <source_dirctory> <destination_directory> <Days>"
+ }
+
+ if [ $# -lt 2 ]
+ then
+    USAGE
+fi
