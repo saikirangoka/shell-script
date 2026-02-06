93afb2aa333a6c800ebe2c5f9049c00a237cd957
saikirangoka <gokasaikiran15@gmail.com>
2026-02-06 14:51:55 +0530

    for

--- 15_Delete_old_logs.sh
+++ 15_Delete_old_logs.sh
@@ -20,7 +20,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscriptlogs/"
 LOG_FILE="echo $0 | cut -d "." -f1 "
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
