f6097b6e833187597b5f31603f1570316b45e533
saikirangoka <gokasaikiran15@gmail.com>
2026-02-06 14:08:25 +0530

    for

--- 15_Delete_old_logs.sh
+++ 15_Delete_old_logs.sh
@@ -1,1 +1,42 @@
 #!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "need sudo access"
+    exit 1
+fi
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/logs/shellscriptlogs/"
+LOG_FILE="echo $0 | cut -d "." -f1 "
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/$TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "installing $2  is  $R failed $N"
+        exit 1
+    else
+        echo "installing $2 is $G success $N"
+    fi
+}
+
+echo "script started executed at : $TIMESTAMP" &>>LOG_FILE_NAME
+
+for package in $@
+do
+    dnf list installed $package &>>LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y &>>LOG_FILE_NAME
+        VALIDATE $? "$package"
+    else
+        echo "$package already installed"
+    fi
+done
