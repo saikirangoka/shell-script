077b0c45a59095c3962700d81aeb2a99c470c284
saikirangoka <gokasaikiran15@gmail.com>
2026-02-06 14:54:19 +0530

    for

--- 15_Delete_old_logs.sh
+++ 15_Delete_old_logs.sh
@@ -17,8 +17,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscriptlogs/"
-LOG_FILE="echo $0 | cut -d "." -f1 "
+LOGS_FOLDER="/var/log/shellscriptlogs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
